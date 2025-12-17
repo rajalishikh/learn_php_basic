ae037066970aedd71870da4dcbc64ae9e00c6240
Raj <nahidujjamanraj@gmail.com>
2025-12-17 10:14:20 +0600

    Run my first php code successfully

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>learn php Basic</title>
+</head>
+<body>
+    <?php
+      echo "hello php";
+      echo "Hi I am first learn php ";
+      echo "MY Target is regular codding ";
+      
+      
+      ?>
+    
+</body>
+</html>
