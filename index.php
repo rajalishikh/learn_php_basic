bc7844e9a8d12c9012bd47113b8fcf9c7c7de68d
MD: Raj Ali Sheikh <138352243+rajalishikh@users.noreply.github.com>
2025-12-22 11:01:26 +0600

    just  learn php

--- index.php
+++ index.php
@@ -11,9 +11,14 @@
    echo "Don't be affraid challanges ";
    echo "যদি তুমি  হতাশ হইএয়া জাও তুমি হেরে জাবা যদি তূমি সাহসী হও জিতে জাবা ";
    echo "বল বীর , 
-   বল উন্নত মম শীর 
-   
-   ";
+   বল উন্নত মম শীর ";
+   echo " code  eat  sleep repait ";
+
+   $my_aim_in_life ="I want to be full stack engineer and all so a teacher"; 
+   echo "Hi raj what is you aim in life ";
+   echo "my aim in life ".$my_aim_in_life ;
+
+
    ?>
     
 </body>
