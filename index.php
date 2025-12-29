2145456b15d3c57a11f817433d2a08022bab288c
MD: Raj Ali Sheikh <138352243+rajalishikh@users.noreply.github.com>
2025-12-29 11:03:58 +0600

    learn_single_quatation_double_quatesiton

--- index.php
+++ index.php
@@ -12,7 +12,15 @@
    echo "My name is Raj alI sheikh </br> ";
    echo"Hani amir is so beautifull <br>";
    echo "Hamja is most good person <br>";
-   echo "Al ways trust yourself <br>"
+   echo "Al ways trust yourself <br>";
+   $text1="Be confidant all ways <br>";
+   $text2="Dont judge a book bye his cover <br>";
+
+   echo"<h2>$text1</h2> <br>";
+   echo "<h1>$text2</h1> <br>";
+   $name = "Linus";
+   echo "<h1>Hello $name</h1>";
+   echo "<h1>Hello $name</h1>";
 
 
    ?>
